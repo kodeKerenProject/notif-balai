dc33a07d9ab5e7ea1dca91a39bcb995dac41726a
ArdhinNaufal <haalozha@gmail.com>
2019-06-12 13:24:12 +0700

    User Table Added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role_id')->unsigned()->nullable();
+            $table->string('negeri');
             $table->rememberToken();
             $table->timestamps();
         });
